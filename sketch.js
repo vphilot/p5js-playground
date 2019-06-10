67fc57be725546f3bbbbc2808b09f95e9a9c23a5
Philot <vphilot@gmail.com>
2019-06-10 16:58:26 -0400

    nothing interesting so far

--- sketch.js
+++ sketch.js
@@ -1,27 +1,23 @@
 var weatherData;
+var d, i = 0;
 
 function setup() {
     createCanvas(200,200);
-    loadJSON("https://api.openweathermap.org/data/2.5/weather?q=London&&appid=cdc1a2cf7a5d1e97671ffec64c6633f1", gotData, 'jsonp');
+    loadJSON("https://api.openweathermap.org/data/2.5/weather?q=Toronto,ca?&appid=24038fb8c6b405ee1ea31e37af50b8b1&?units=metric", gotData, 'jsonp');
 }
 
 function gotData(data){
     weatherData = data;
+    print(weatherData);
+    d = weatherData.main.temp - 273.15;
 }
 
 function draw() {
     background(0);
-    // stroke (255);
-    // line(x, 0, x, height);
-    // x++;
-    // if (x > width) {
-    //     x = 0;
-    // }
     if (weatherData){
-        randomSeed(4);
-        print(weatherData);
-        // for (var i = 0; i < spaceData.number; i++) {
-        //     ellipse(random(width), random(height), 16, 16)
-        // }
+        ellipse(50, 100, d + i, d);
+        ellipse(150, 100, weatherData.main.humidity, weatherData.main.humidity);
+        i+=0.5;
     }
+
 }
